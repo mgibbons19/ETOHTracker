6625a4a8d42976d4f6ec15675a0e9bd915111098
mgibbons19 <mgibbons19@gmail.com>
2026-02-06 16:04:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // After deploying your script, it will look like:
 // https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec
 const API_URL = https://script.google.com/macros/s/AKfycbyiAThSjWa61NUelZ8NSvCecq6Oag9BLra0vGkzJo_84xLIZ_kROr6lHJ6xMCQQe6bu/exec;
-
+                
 // Check if API URL is configured
 if (API_URL === https://script.google.com/macros/s/AKfycbyiAThSjWa61NUelZ8NSvCecq6Oag9BLra0vGkzJo_84xLIZ_kROr6lHJ6xMCQQe6bu/exec) {
     console.warn('⚠️ API URL not configured! Please update config.js with your Google Apps Script deployment URL.');
